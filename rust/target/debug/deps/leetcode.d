c4a330d264de4b1340a318777496577703097ab6
Qustio <qustioof@gmail.com>
2022-09-27 16:31:52 +0300

    Ignore target dir for rust

--- rust/target/debug/deps/leetcode.d
+++
@@ -1,5 +0,0 @@
-E:\Projects\Code-Samples\rust\target\debug\deps\leetcode.exe: src\main.rs
-
-E:\Projects\Code-Samples\rust\target\debug\deps\leetcode.d: src\main.rs
-
-src\main.rs:
--- rust/target/debug/leetcode.d
+++
@@ -1,1 +0,0 @@
-E:\Projects\Code-Samples\rust\target\debug\leetcode.exe: E:\Projects\Code-Samples\rust\src\main.rs
